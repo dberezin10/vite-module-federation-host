107755837b969d786bb54b2c97a229e19b31fd7e
denisberezin <dberezin@ippontech.ru>
2024-07-23 14:17:42 +0300

    add vite-module-federation-host

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+// @ts-ignore
+import RemoteComponent from 'remote/RemoteComponent';
+
+const App = () => {
+
+  return (
+    <h1>
+        host
+        <RemoteComponent />
+    </h1>
+  )
+}
+
+export default App
